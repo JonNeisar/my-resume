b81cbd8c72c3e521f3d22453c8c819e5ccb23f98
Jonathan Iglesias Belmonte <jonatan.iglesias.belmonte@gmail.com>
2022-10-04 19:38:18 +0200

    Removes console log. Avoids hide again after show animation

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
         }
     });
 });
